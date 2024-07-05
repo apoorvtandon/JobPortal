655704702df33aae5a0b35f7ab34e7e10868e4c6
Aryan Jaiswal <theguruaryan@gmail.com>
2024-07-06 02:49:04 +0530

    adding filters to the jobs section

--- src/app/jobs/page.js
+++ src/app/jobs/page.js
@@ -1,33 +1,43 @@
-import { fetchJobApplicationsForCandidate, fetchJobApplicationsForRecruiter, fetchJobsForCandidateAction, fetchJobsForRecruiterAction, fetchProfileAction } from "@/actions";
-import JobListing from "@/components/job-listing"
-import { currentUser } from '@clerk/nextjs/server';
+import {
+  fetchJobApplicationsForCandidate,
+  fetchJobApplicationsForRecruiter,
+  fetchJobsForCandidateAction,
+  fetchJobsForRecruiterAction,
+  fetchProfileAction,
+  createFilterCategoryAction,
+} from "@/actions";
+import JobListing from "@/components/job-listing";
+import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
+async function JobsPage({ searchParams }) {
+  console.log(searchParams);
+  const user = await currentUser();
+  if (!user) {
+    redirect("/sign-in");
+  }
+  const profileInfo = await fetchProfileAction(user?.id);
+  const jobList =
+    profileInfo?.role === "candidate"
+      ? await fetchJobsForCandidateAction(searchParams)
+      : await fetchJobsForRecruiterAction(user?.id);
 
- 
+  const getJobApplicationList =
+    profileInfo?.role === "candidate"
+      ? await fetchJobApplicationsForCandidate(user?.id)
+      : await fetchJobApplicationsForRecruiter(user?.id);
 
-async function JobsPage ()  {
-    const user = await currentUser();
-    if(!user)
-      {
-        redirect('/sign-in')
-      }
-    const profileInfo = await fetchProfileAction(user?.id);
-    const jobList = profileInfo?.role === 'candidate'?
-    await fetchJobsForCandidateAction()
-    :await fetchJobsForRecruiterAction(user?.id)
-    
-    const getJobApplicationList = profileInfo?.role === 'candidate'
-    ?await fetchJobApplicationsForCandidate(user?.id) 
-    :await fetchJobApplicationsForRecruiter(user?.id)
+  const fetchFilterCategories = await createFilterCategoryAction();
+
   return (
     <JobListing
-    user = {JSON.parse(JSON.stringify(user))}
-    profileInfo={profileInfo}
-    jobList={jobList}
-    jobApplications = {getJobApplicationList}
+      user={JSON.parse(JSON.stringify(user))}
+      profileInfo={profileInfo}
+      jobList={jobList}
+      jobApplications={getJobApplicationList}
+      filterCategories={fetchFilterCategories}
     />
-  )
+  );
 }
 
-export default JobsPage
+export default JobsPage;
